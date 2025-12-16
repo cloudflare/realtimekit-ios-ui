c5897aa5dba9f8a36b89f1424783ef57f5b202c6
dyte-devel <76557270+dyte-devel@users.noreply.github.com>
2025-12-16 07:45:37 +0000

    chore(release): update RealtimeKitUI to 0.5.6

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/dyte-in/RealtimeKitCoreiOS.git",
-            from: "1.5.6"
+            from: "1.5.7"
         ),
     ],
     targets: [
